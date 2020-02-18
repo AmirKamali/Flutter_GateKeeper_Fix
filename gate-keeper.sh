18ba9b8096edbf2c6b37ce00d9f8d8e2f15e997c
Amir -Office's iMac <ak@3dsafety.com.au>
2020-02-18 15:31:34 +1100

    add gen_snapshot

--- gate-keeper.sh
+++ gate-keeper.sh
@@ -17,7 +17,7 @@ sudo spctl --add $fpath
 # idevice_id
 fpath="$FLPATH/bin/cache/artifacts/libimobiledevice/idevice_id"
 echo "Adding exception for $fpath"
-sudo spctl --add $fpath
+sudo spctl --add "$FLPATH/bin/cache/artifacts/libimobiledevice/idevice_id"
 #
 # ideviceinfo
 fpath="$FLPATH/bin/cache/artifacts/libimobiledevice/ideviceinfo"
@@ -39,3 +39,18 @@ fpath="$FLPATH/bin/cache/artifacts/libimobiledevice/idevicesyslog"
 echo "Adding exception for $fpath"
 sudo spctl --add $fpath
 
+# gen_snapshot
+echo "Adding exception for gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/android-x64-release/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/android-arm64-profile/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/android-arm64-release/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/android-x64-profile/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/android-arm-profile/darwin-x64/gen_snapshot"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/ios/gen_snapshot_arm64"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/ios-release/gen_snapshot_arm64"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/ios-profile/gen_snapshot_arm64"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/ios-release/gen_snapshot_armv7"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/ios-profile/gen_snapshot_armv7"
+sudo spctl --add $fpath "$FLPATH/bin/cache/artifacts/engine/ios/gen_snapshot_armv7"
